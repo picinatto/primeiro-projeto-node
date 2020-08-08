9b6d869c199c4adae9535014fc2f3a3ee773e1af
Ricardo <ederli.picinatto@gmail.com>
2020-08-08 10:52:18 -0300

    Created appointments interface

--- src/modules/appointments/infra/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/repositories/AppointmentsRepository.ts
@@ -1,16 +1,19 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepositories';
+
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     // Find one extends from repository and receives a where condition
     const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
