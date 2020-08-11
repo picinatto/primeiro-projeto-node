f7bc71ee196a68833e293d891dc2e27be7f02eff
Ricardo <ederli.picinatto@gmail.com>
2020-08-10 21:46:07 -0300

    refatored Appointments repositoy to use interface

--- src/modules/appointments/infra/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/repositories/AppointmentsRepository.ts
@@ -1,15 +1,19 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepositories';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
     // Find one extends from repository and receives a where condition
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
