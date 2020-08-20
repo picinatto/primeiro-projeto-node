80a67492657dacb7bcead0aa918dedbd10fb01cb
Ricardo <ederli.picinatto@gmail.com>
2020-08-19 21:22:39 -0300

    authenticate user test

--- src/modules/users/services/AuthentcateUserService.spec.ts
+++ src/modules/users/services/AuthentcateUserService.spec.ts
@@ -1,11 +1,24 @@
-import AppError from '@shared/errors/AppError';
-
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import AuthenticaUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
-  it('should be able to authenticate user', async () => {
+  it('should be able to authenticate the user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const authenticateUser = new AuthenticaUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
   });
 });
