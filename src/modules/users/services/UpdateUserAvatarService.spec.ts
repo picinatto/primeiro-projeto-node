5bc6c815561f81deca837abcdef41a2cef6ced55
Ricardo <ederli.picinatto@gmail.com>
2020-08-20 10:14:01 -0300

    finished all update avatar tests

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
@@ -24,5 +25,56 @@ describe('UpdateUserAvatar', () => {
     });
 
     expect(user.avatar).toBe('updated-test');
+  });
+
+  it('should not be able to update avatar of non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFileName: 'updated-test',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete the file if the avatar is existent', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    // Createing spy to know if the delete file function was triggered
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFileName: 'file1.jpg',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFileName: 'file2.jpg',
+    });
+
+    // Check if the function deleteFile was called with file1.jpg parameter
+    expect(deleteFile).toHaveBeenCalledWith('file1.jpg');
+    // Check if the avatar was updated to the user
+    expect(user.avatar).toBe('file2.jpg');
   });
 });
