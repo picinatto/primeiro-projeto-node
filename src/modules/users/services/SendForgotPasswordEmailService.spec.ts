dd1dd62efb65083d2f99fd38cd81622379186fac
Ricardo <ederli.picinatto@gmail.com>
2020-08-21 12:04:51 -0300

    Started SendForgotPasswordService

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,15 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+    );
+
+    await
+  });
+});
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,22 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+// import User from '@modules/users/infra/typeorm/entities/Users';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {}
+}
+
+export default SendForgotPasswordEmailService;
