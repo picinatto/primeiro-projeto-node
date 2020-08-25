e50a0317623a48d062700f9f9e280b4a13997423
Ricardo <ederli.picinatto@gmail.com>
2020-08-25 08:00:49 -0300

    should not  send recover password for non existin

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,7 +1,8 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
 describe('SendForgotPasswordEmail', () => {
@@ -25,7 +26,20 @@ describe('SendForgotPasswordEmail', () => {
 
     sendForgotPasswordEmail.execute({ email: 'johndoe@example.com' });
 
-    // Check if the function deleteFile was called with file1.jpg parameter
     expect(sendMail).toHaveBeenCalled();
+  });
+
+  it('should not be able to recover a non existent user password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await expect(
+      sendForgotPasswordEmail.execute({ email: 'johndoe@example.com' }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
