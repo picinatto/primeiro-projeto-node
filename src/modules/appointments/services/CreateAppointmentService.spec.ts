6ccbc7b3241c9b82aaed2332a111870da2ba4bea
Ricardo <ederli.picinatto@gmail.com>
2020-09-22 18:31:02 -0300

    fixed test create appointment service same time

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -20,31 +20,35 @@ describe('CreateAppointment', () => {
 
     const appointment = await createAppointment.execute({
       date: new Date(2020, 4, 10, 13),
-      provider_id: 'user_id',
-      user_id: '123123',
+      provider_id: 'provider_id',
+      user_id: 'user_id',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('user_id');
+    expect(appointment.provider_id).toBe('provider_id');
   });
 
-  // it('should not be able to create two appointments on the same time', async () => {
-  //   const appointmentDate = new Date(2020, 4, 10, 11);
+  it('should not be able to create two appointments on the same time', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
 
-  //   await createAppointment.execute({
-  //     date: appointmentDate,
-  //     provider_id: 'user_id',
-  //     user_id: '123123',
-  //   });
+    const appointmentDate = new Date(2020, 10, 10, 16);
 
-  //   await expect(
-  //     createAppointment.execute({
-  //       date: appointmentDate,
-  //       provider_id: 'user_id',
-  //       user_id: '123123',
-  //     }),
-  //   ).rejects.toBeInstanceOf(AppError);
-  // });
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: 'provider_id',
+        user_id: 'user_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 
   it('should not be able to create an appointment on a past date', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
@@ -55,7 +59,7 @@ describe('CreateAppointment', () => {
       createAppointment.execute({
         date: new Date(2020, 4, 10, 11),
         provider_id: 'user_id',
-        user_id: '123123',
+        user_id: 'provider_id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
