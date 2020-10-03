35342cead121d5b525b62fa5bd62c61ae581fff0
Ricardo <ederli.picinatto@gmail.com>
2020-10-03 18:10:33 -0300

    upload files to S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,28 +3,53 @@ import path from 'path';
 // Import crypto to use the randon hash generator
 import crypto from 'crypto';
 // Import multer for configuring the file upload storage
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
+
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  // Create the storage object that will hold information
-  // about where to store and with which name
-  storage: multer.diskStorage({
-    // Use path to get safely the temp folder location
-    destination: tmpFolder,
-    // Use the filename to configure with which name the file
-    // will be stored on the destination
-    filename(request, file, callback) {
-      // Create a hash to generate a unique string
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      // Get the original file name uploaded by the user
-      const fileName = `${fileHash}-${file.originalname}`;
-      // return the call back with error:null and FileName
-      return callback(null, fileName);
-    },
-  }),
-};
+  multer: {
+    // Create the storage object that will hold information
+    // about where to store and with which name
+    storage: multer.diskStorage({
+      // Use path to get safely the temp folder location
+      destination: tmpFolder,
+      // Use the filename to configure with which name the file
+      // will be stored on the destination
+      filename(request, file, callback) {
+        // Create a hash to generate a unique string
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        // Get the original file name uploaded by the user
+        const fileName = `${fileHash}-${file.originalname}`;
+        // return the call back with error:null and FileName
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+  },
+} as IUploadConfig;
---
+++ src/shared/container/providers/StorageProvider/implementations/S3StrorageProvider.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+import aws, { S3 } from 'aws-sdk';
+import uploadConfig from '@config/upload';
+import IStorageProvider from '../models/IStorageProvider';
+
+class DiskStorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: 'us-east-1',
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalPath, {
+      encoding: 'utf-8',
+    });
+
+    await this.client
+      .putObject({
+        Bucket: 'app-gobarber',
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+      })
+      .promise();
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: 'app-gobarber',
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export default DiskStorageProvider;
